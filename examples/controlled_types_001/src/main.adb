f395fbf65e82b020ee6c9c033942e44d480750ce
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-03-17 13:58:05 +0100

    Made the example a little bit easier to understand

--- examples/controlled_types_001/src/main.adb
+++ examples/controlled_types_001/src/main.adb
@@ -8,14 +8,14 @@ begin
 
    Controlled_Door := Factory.Create_Controlled_Door("door1");
 
-   declare
-      Controlled_Door2: Factory.Controlled_Door;
-   begin
-      Ada.Text_IO.Put_Line("Start Declare");
-      Controlled_Door2 := Factory.Create_Controlled_Door("door2");
-
-      Ada.Text_IO.Put_Line("End Declare");
-   end;
-
+--   declare
+--      Controlled_Door2: Factory.Controlled_Door;
+--   begin
+--      Ada.Text_IO.Put_Line("Start Declare");
+--      Controlled_Door2 := Factory.Create_Controlled_Door("door2");
+--
+--      Ada.Text_IO.Put_Line("End Declare");
+--   end;
+--
    Ada.Text_IO.Put_Line("End Main");
 end Main;
